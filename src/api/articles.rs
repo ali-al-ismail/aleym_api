0b189b391ee414520badddcac0f01a5118472342
ali-al-ismail <2220005098@iau.edu.sa>
2026-03-24 14:21:30 +0300

    feat: implement article handlers

--- src/api/articles.rs
+++ src/api/articles.rs
@@ -1,9 +1,130 @@
-use axum::extract::Path;
+use crate::appstate::AppState;
+use aleym_core::db::{
+	DirectoryBasedNewsFilter, DirectoryOrCategoriesBasedNewsFilter, NewsFilter, SortOrder, TIME_MAX, TIME_MIN,
+	time::OffsetDateTime, uuid::Uuid,
+};
+use axum::{
+	Json,
+	extract::{Path, Query, State},
+};
+use serde::{Deserialize, Serialize};
+use std::sync::Arc;
 
-pub async fn get_articles() -> &'static str {
-	"GET /articles"
+#[derive(Serialize, Deserialize)]
+pub struct SimpleArticle {
+	pub id: Uuid,
+	pub source: Uuid,
+	pub title: String,
+	pub uri: Option<String>,
+	pub last_fetched_at: i64,
+	pub is_read: bool,
 }
 
-pub async fn get_article_by_id(Path(id): Path<u32>) -> String {
-	format!("GET Article ID: {id}")
+#[derive(Serialize, Deserialize)]
+pub struct Article {
+	pub id: Uuid,
+	pub source: Uuid,
+	pub title: String,
+	pub uri: Option<String>,
+	pub content: Option<String>,
+	pub last_fetched_at: i64,
+	pub published_at: Option<i64>,
+	pub is_read: bool,
+}
+
+#[derive(Deserialize)]
+#[serde(rename_all = "lowercase")]
+pub enum SortOrderQuery {
+	Asc,
+	Desc,
+}
+
+#[derive(Deserialize)]
+pub struct ArticleQuery {
+	pub limit: Option<u64>,
+	pub after: Option<i64>,
+	pub before: Option<i64>,
+	pub sort_order: Option<SortOrderQuery>,
+	pub source_id: Option<Uuid>,
+	pub category_id: Option<Uuid>,
+}
+
+// no idea if this works, for now it doesn't allow for filtering multiple categories at the same time even though the core supports it
+// the filter gives priority to source filtering over category filtering
+// if neither source or category filtering is active then defaults to grabbing everything from the root directory (basically just grab every article that matches the other time and limit filters)
+pub async fn get_articles(
+	State(state): State<Arc<AppState>>,
+	Query(params): Query<ArticleQuery>,
+) -> Json<Vec<SimpleArticle>> {
+	let cursor_after = OffsetDateTime::from_unix_timestamp(params.after.unwrap_or(TIME_MIN.unix_timestamp())).unwrap();
+
+	let cursor_before =
+		OffsetDateTime::from_unix_timestamp(params.before.unwrap_or(TIME_MAX.unix_timestamp())).unwrap();
+
+	let limit = params.limit.unwrap_or(50);
+
+	let sort_order = match params.sort_order.unwrap_or(SortOrderQuery::Asc) {
+		SortOrderQuery::Asc => SortOrder::Asc,
+		SortOrderQuery::Desc => SortOrder::Desc,
+	};
+
+	let filter = if let Some(source_id) = params.source_id {
+		NewsFilter::Source(source_id)
+	} else if let Some(category_id) = params.category_id {
+		NewsFilter::DirectoryOrCategories(DirectoryOrCategoriesBasedNewsFilter {
+			directory: None,
+			categories: vec![category_id],
+		})
+	} else {
+		let root_dir_uuid = state
+			.repr
+			.storage
+			.get_root_directories()
+			.await
+			.unwrap()
+			.pop()
+			.unwrap()
+			.id;
+		NewsFilter::DirectoryOrCategories(DirectoryOrCategoriesBasedNewsFilter {
+			directory: Some(DirectoryBasedNewsFilter {
+				parent_directory: root_dir_uuid,
+				recursive: true,
+			}),
+			categories: vec![],
+		})
+	};
+
+	let articles = state
+		.repr
+		.storage
+		.get_news_with_source_filter(filter, sort_order, cursor_after, cursor_before, limit)
+		.await
+		.unwrap()
+		.into_iter()
+		.map(|a| SimpleArticle {
+			id: a.id,
+			source: a.source,
+			title: a.title,
+			uri: a.uri,
+			last_fetched_at: a.last_fetched_at.unix_timestamp(),
+			is_read: a.is_read,
+		})
+		.collect();
+
+	Json(articles)
+}
+
+// UNTESTED
+pub async fn get_article_by_id(State(state): State<Arc<AppState>>, Path(id): Path<Uuid>) -> Json<Article> {
+	let article = state.repr.storage.get_news(id).await.unwrap();
+	Json(Article {
+		id: article.id,
+		source: article.source,
+		title: article.title,
+		uri: article.uri,
+		content: article.content,
+		last_fetched_at: article.last_fetched_at.unix_timestamp(),
+		published_at: article.published_at.map(|time| time.unix_timestamp()),
+		is_read: article.is_read,
+	})
 }
