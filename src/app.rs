8887873f22f68739e3f456154996b7f5faeddbbf
ali-al-ismail <2220005098@iau.edu.sa>
2026-03-24 07:30:02 +0300

    refac: cleaner way of setting up routes

--- src/app.rs
+++ src/app.rs
@@ -12,25 +12,17 @@ use tower_http::services::{ServeDir, ServeFile};
 
 pub fn build(appstate: Arc<AppState>) -> axum::Router {
 	let source_routes = axum::Router::new()
-		.route("/sources", get(get_sources))
-		.route("/sources/{id}", get(get_source_by_id))
-		.route("/sources", post(create_source))
-		.route("/sources/{id}", put(update_source))
+		.route("/sources", get(get_sources).post(create_source))
+		.route("/sources/{id}", get(get_source_by_id).put(update_source))
+		.route("/sources/{id}/categories", get(get_source_categories))
 		.route(
 			"/sources/{source_id}/categories/{category_id}",
-			post(link_source_to_category),
-		)
-		.route(
-			"/sources/{source_id}/categories/{category_id}",
-			delete(unlink_source_to_category),
-		)
-		.route("/sources/{id}/categories", get(get_source_categories));
+			post(link_source_to_category).delete(unlink_source_to_category),
+		);
 
 	let categories_routes = axum::Router::new()
-		.route("/categories", get(get_categories))
-		.route("/categories", post(create_category))
-		.route("/categories/{id}", put(update_category))
-		.route("/categories/{id}", delete(delete_category))
+		.route("/categories", get(get_categories).post(create_category))
+		.route("/categories/{id}", put(update_category).delete(delete_category))
 		.route("/categories/{id}/sources", get(get_sources_by_category));
 
 	let articles_routes = axum::Router::new()
@@ -40,10 +32,8 @@ pub fn build(appstate: Arc<AppState>) -> axum::Router {
 	let api_routes = source_routes.merge(categories_routes).merge(articles_routes);
 
 	axum::Router::new()
-		// api routes
 		.nest("/api", api_routes)
 		.route("/events", get(events_handler))
 		.with_state(appstate)
-		// static react files
 		.fallback_service(ServeDir::new("web/dist").not_found_service(ServeFile::new("web/dist/index.html")))
 }
